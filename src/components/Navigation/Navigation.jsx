e829c94aae1ff80d05e02c9be4c1d93aa00112a6
spell-landon <spell.landon@gmail.com>
2021-12-28 15:32:53 -0600

    Add Navigation with styling

---
+++ src/components/Navigation/Navigation.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import styles from './Navigation.module.css';
+
+function Navigation(props) {
+  return (
+    <nav>
+      <ul>
+        <li className={styles.apple}>
+          <i class='fab fa-apple'></i>
+        </li>
+        <li>Store</li>
+        <li>Mac</li>
+        <li>iPad</li>
+        <li>iPhone</li>
+        <li>Watch</li>
+        <li>AirPods</li>
+        <li>TV & Home</li>
+        <li>Only on Apple</li>
+        <li>Accessories</li>
+        <li>Support</li>
+        <li>
+          <i className='fas fa-search'></i>
+        </li>
+        <li>
+          <i className='fas fa-shopping-bag'></i>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navigation;
